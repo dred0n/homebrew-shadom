170bb5b5e28b9dabe3dc0941326aa045e14de1e3
senseable <dondre+witcebs@gmail.com>
2026-05-06 08:36:10 -0500

    Add shadom formula

---
+++ Formula/shadom.rb
@@ -0,0 +1,19 @@
+class Shadom < Formula
+  desc "Agent-first browser automation CLI"
+  homepage "https://github.com/dred0n/shadom"
+  url "https://github.com/dred0n/shadom.git",
+      tag:      "v0.1.0",
+      revision: "070bac39a40b668c7f207aa1d3e900256167c3c9"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--path", ".", "--root", prefix
+  end
+
+  test do
+    assert_match "agent-first browser automation CLI", shell_output("#{bin}/shadom --help")
+    assert_predicate bin/"stealth", :exist?
+  end
+end
